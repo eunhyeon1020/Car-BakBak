772efef1283d7fd201dfecc28e9ff46953cdfdec
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-10-21 17:52:56 +0900

    footer 추가 및 수정 (html, css)

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="footer"> 푸터입니다. </div>
-
+  <div id="footer">
+    <div class="ft_content">
+      <div class="ft_logo"></div>
+      <ul>
+        <li>사업명: 차박박 | 조장: 박소정 팀원: 김은현</li>
+        <li>문의 번호: 02-2212-0082 | 주소: 서울특별시 동대문구 망우로 82</li>
+        <li>대표 메일: car-bakbak@shu-capstone.com</li>
+        <p>© 2024 Car-Bakbak. This website is a graduation project.</p>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {};
 </script>
 
-<style scoped>
-  div#footer {
-    height: 60px;
-    border: 1px solid red;
-  }
-</style>
+<style scoped></style>
