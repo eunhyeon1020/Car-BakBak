9ad755f4bb2347f5f9368ff0bdb07658a33b0c9c
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-10-21 17:54:36 +0900

    메인 배너 자바스크립트로도 변경가능(방법)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div id="app">
-
     <Header></Header>
-    
-    <router-view></router-view>
-    
-    <Footer></Footer>
 
+    <router-view></router-view>
+
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-  import Header from "@/components/Header.vue";
-  import Footer from "@/components/Footer.vue"
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
-  export default {
-    components: { Header, Footer },
-  }
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+  mounted() {
+    this.footerHeight();
+  },
+  methods: {
+    footerHeight() {
+      /* 
+        나중에 여기에
+        .intro-contain 클래스 다 가져와서 몇개인지 length 추출.
+        #footer 아이디로 document 가져와서 footer.style.top = introContain의 length * 100vh
+      */
+    },
+  },
+};
 </script>
