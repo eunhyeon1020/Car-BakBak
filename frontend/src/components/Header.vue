0b00f152c6d48a5ed2029e0f65da93a31736dbce
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-10-21 17:52:31 +0900

    header 추가 및 수정

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="header">
-    헤더 영역
-    <button @click="btnClick">서브 페이지로 이동</button>
-    <router-link :to="'sub'">서브 페이지로 이동2</router-link>
-
+    <div class="gnb">
+      <div class="header-left">
+        <div class="gnb-logo"></div>
+        <div class="top-menu">
+          <ul>
+            <li>
+              <i class="ico about-menu"></i>
+              <span>소개</span>
+            </li>
+            <li>
+              <i class="ico map-menu"></i>
+              <span>지도</span>
+            </li>
+            <li>
+              <i class="ico store-menu"></i>
+              <span>스토어</span>
+            </li>
+            <li>
+              <i class="ico notice-menu"></i>
+              <span>고객지원</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="header-right">
+        <div class="login"><p>로그인</p></div>
+        <div>|</div>
+        <div class="sign-up"><p>회원가입</p></div>
+        <div class="user" style="display: none"><p>김은현 님</p></div>
+        <div style="display: none">|</div>
+        <div class="my-page" style="display: none"><p>마이페이지</p></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    btnClick() {
-      console.log("this  ::", this);
-      this.$router.push('sub');
-    }
-  }
-}
+export default {};
 </script>
