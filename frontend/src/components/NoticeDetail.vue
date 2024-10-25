dc32540b2f3542bc1bdf3fe42701722b114c8ff5
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-10-25 15:28:22 +0900

    notice html 제목 추가 및 css 추가

--- frontend/src/components/NoticeDetail.vue
+++ frontend/src/components/NoticeDetail.vue
@@ -1,7 +1,20 @@
 <template>
-  <div id="noticeDetail">
+  <div id="noticedetail">
     <div class="ntDetail-contain">
       <h1>공지사항</h1>
+      <div class="ntDetail-content">
+
+        <div class="ntDetail-title">
+          웹 사용 공지사항 
+
+        </div>
+
+
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+
+</style>
