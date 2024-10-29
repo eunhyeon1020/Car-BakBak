ad1df3411e9415f5c0a06fe904827191a00f2133
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-10-29 15:46:01 +0900

    login html 구조 수정 & css 디테일 수정

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,44 +1,75 @@
 <template>
-  <div class="login">
-  <div class="login-box">
+  <div id="login">
+    <div class="login-box">
+
+      <router-link :to="'main'">
         <div class="login-logo"></div>
-        <form id="login-form">
-          <div class="login-top-text">
-            <h3>로그인</h3>
+      </router-link>
+
+      <!-- 로그인 입력 부분 -->
+      <form id="login-form" @submit.prevent="formSubmit">
+        <div class="login-top-text">
+          <h3>로그인</h3>
+          <div>
             <span>신규 이용자신가요?</span>
-            <span style="color: #016FB6;">계정 만들기</span>
+            <router-link :to="'sign'">
+              <span style="color: #016FB6;">계정 만들기</span>
+            </router-link>
           </div>
+          
           <input type="text" name="userID" placeholder="아이디 입력" />
           <input type="password" name="userPassword" placeholder="비밀번호 입력"/>
-          <input type="submit" value="로그인">
-          <div class="find-text">
-            <p>비밀번호 찾기</p>
-            <div>|</div>
-            <p>아이디 찾기</p>
-          </div>
-            
-        </form>
+          <input type="submit" value="로그인" style="cursor: pointer;">
+          
+        </div>
+
+        <div class="find-text">
+          <p>비밀번호 찾기</p>
+          <p>|</p>
+          <p>아이디 찾기</p>
+        </div>          
+      </form>
     </div>
   </div>
 </template>
-<script>
-  export default {
 
+<script>
+// import { router } from '@/routes/routes';
+
+  export default {
+    data: () => ({
+      success: true,
+    }),
+    methods: {
+      formSubmit() {
+        // const form = document.getElementById('login-form');
+        // const data = {
+        //   userId : 'XXX',
+        //   userPw : 'XXXXXXXXXX',
+        // }
+        // await res = $axios('http://localhost:18080/login', data);
+        if (this.success) {
+          confirm('로그인 창으로 이동합니다.'); // 확인 : true, 취소 : false
+          this.$router.push({path: '/main'});
+        } else {
+          this.$router.push({path: '/login'});
+          alert('아이디 패스워드를 확인해주세요.');
+        }
+      },
+    }
+    
+    /* 데이터 내려받는 예시
+
+      {
+        data: {
+          userId: 'user123',
+          userNm: '남궁은현',
+          userRate: '마스터',
+        },
+        success: true,
+        statusCd: 202 / 404
+      }
+
+    */
   }
 </script>
-
-<style scoped>
-* {padding: 0;margin: 0 auto;border: none;}
-body {font-size: 14px;font-family: 'Roboto', sans-serif;}
-.login {width: 100%;height: 100vh;background:url(../assets/images/loginsign-bg.png) fixed center center / cover;}
-.find-text {font-size: 13px; color: #9B9A9A;display: flex; padding: 0 100px;}
-.login-top-text {text-align: left;margin-bottom: 20px}
-.login-top-text>h3 {font-size: 30px; color: #000000; margin-bottom: 10px}
-.login-top-text > span {font-size: 14px; color: #545454; margin-right: 5px;}
-.login-logo {background-image: url("../assets/images/차박박.png"); background-size: 256px 103px;width: 256px;height: 103px}
-.login-box {width: 540px; height: 100%;padding: 40px;box-sizing: border-box;margin-top: 30px;}
-.login > h2 {font-size: 50px;font-weight: 1000;color: #8129e0;margin-bottom: 30px;margin-top: 100px;text-align: center;}
-#login-form {border-radius: 15px;padding: 40px;margin-top: 25px;box-shadow:0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);background-color: #ffffff;}
-#login-form > input {width: 100%; height: 40px;margin-bottom: 15px;border-radius: 5px;box-sizing: border-box;padding: 0 10px;background-color: #f5f5f5;}
-#login-form > input[type="submit"] {background-color: #243516;color: white;height: 42px;}
-</style>
