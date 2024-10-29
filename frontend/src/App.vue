0924111bcd7b398ec0832acd9d8b58d89c144b95
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-10-29 14:25:14 +0900

    map, intro에 footer 조절 코드 추가 & login에 header 삭제 코드 추천

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,48 +1,72 @@
 <template>
   <div id="app">
-    <Header></Header>
+    <Header v-if="isShow"></Header>
 
     <router-view></router-view>
 
-    <Footer></Footer>
+    <Footer v-if="isShow"></Footer>
   </div>
 </template>
+
+<script setup>
+  // var variable = 10;
+</script>
 
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
+  // import 한 컴포넌트를 template에서 사용할 수 있게 해주는 부분 ( 태그로 사용 가능 )
   components: {
     Header,
     Footer,
   },
+
+  // 변수의 초기 값을 지정해주는 부분
   data:() => ({
     currentPath : '',
+    isShow: true, // Boolean : true / false
   }),
+  
+  // 데이터가 수시로 
   watch: {
-   $route(to) {
-      this.currentPath = to.path;
+    // 페이지 host 뒤에 오는 파라미터가 변화될 때 실행 (주소 뒤 /파라미터)
+    $route(to) {  // to 는 route라는 라이브러리(모듈)
+      this.isShow = true;
+      this.currentPath = to.path; // '/map'
 
       window.scrollTo(0,0);
 
       this.$nextTick(() => {
-        const useSafeElement = document.querySelector('#useSafe');
-        
-        const footer = document.querySelector('#footer');
-        if (this.currentPath === '/intro' || this.currentPath === '/main'){
-         footer.style.top = '0';
-        } else footer.style.top = '';
 
-        if (useSafeElement) {  
+        const useSafeElement = document.querySelector('#useSafe');
+        const footer = document.querySelector('#footer');      
+
+        if (this.currentPath === '/intro' || this.currentPath === '/main'){
+          footer.style.top = '0';
+        } 
+        else {
+          footer.style.top = ''
+        }
+
+        // #useSafe 라는 엘리먼트가 존재할 때! true일 때!
+        if (useSafeElement) {
           if (this.currentPath === '/intro') {
-            const topPosition = useSafeElement.getBoundingClientRect().top + window.scrollY; // 요소의 top 위치 계산
+            // 요소의 top 위치 계산
+            const topPosition = useSafeElement.getBoundingClientRect().top + window.scrollY; 
             window.scrollTo({ top: topPosition, behavior: 'smooth' });
           }
+        }
+
+        // 주소 파라미터가 "/login"일 때
+        if (this.currentPath === '/login'){
+          this.isShow = false;
         }
       });
     },
   },
+
   methods: {
   }
 };
