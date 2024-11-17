9f5bf90042011db91bc0e7078846a476b9ab5c74
parksojeong <thwjdrudtn1212@naver.com>
2024-11-17 12:43:27 +0900

    회원가입 반응형

--- frontend/src/components/Sign.vue
+++ frontend/src/components/Sign.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="sign">
+    <div class="testdiv">
     <div class="sign-logo">
       <router-link :to="'main'">
         <div class="sign-logo-img"></div>
@@ -12,6 +13,7 @@
           <div>
             <span>차박박과 함께 여행을 떠날 준비 되셨나요?</span>
           </div>
+        </div> 
           <div class="sign-input-area">
             <input class="sign-top" type="text" name="uerID" placeholder="아이디">
             <input class="sign-middle" type="password" name="userPassword" placeholder="비밀번호">
@@ -26,16 +28,19 @@
               </select> -->
               <div class="sign-phone-area">
                 <input class="sign-phoneNum" type="text" name="phoneNum" placeholder="전화번호(-제외)">
-                <button class="verify-btn" type="button">인증</button>
-                <button class="verify-btn" type="button">확인</button>
+                <div class="testdiv-btn">
+                  <button class="verify-btn" type="button">인증</button>
+                <!-- <button class="verify-btn" type="button">확인</button> -->
+                </div>
               </div>
             </div>
             <input class="sign-bottom" type="text" placeholder="인증번호">
             <button type="submit" class="signup-btn">회원가입</button>
           </div>
-        </div>         
+                
       </form>
     </div>
+  </div>
   </div>
 </template>
 <script>
