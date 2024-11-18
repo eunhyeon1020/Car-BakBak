78faf4ec276ebd0b01ca0ebd7b2886406f8c050c
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-11-18 17:56:24 +0900

    axios 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from '@/routes/routes'
 
+import axios from 'axios';
+
 
 /* css import 부분 */
 import "@/assets/css/style.css"
 
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
 
