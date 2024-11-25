0424fdda831a9ff63e917a2f7c54364b5eb7e853
eunhyeon1020 <eunhyeon1734@gmail.com>
2024-11-25 11:12:00 +0900

    error html, css 추가 및 반응형 처리

---
+++ frontend/src/components/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="error">
+    
+    <div class="error-contain">
+      
+      <img class="errorGIF" src="../assets/images/errorGIF.gif" >
+      <div class="error-content">
+        <div>죄송합니다.</div>
+        <div>로그인 후, 본 페이지는 이용이 가능합니다.</div>
+        <div class="homeBtn">
+          <div><router-link :to="'/main'">홈으로</router-link></div>
+        </div>
+      </div>
+      
+    </div>
+
+  </div>
+</template>
